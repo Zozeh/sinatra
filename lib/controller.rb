9be87c6fb42bc465df85389ef52eb2d71f827263
zozeh <joseonetouch8@gmail.com>
2019-01-29 08:07:10 +0100

    modidier

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,17 +1,6 @@
 
 =begin
 class ApplicationController < Sinatra::Base
-get '/gossips/new/' do
-  erb :new_gossip
-end
- 
-end
-=end
-
-
-
-=begin
-class ApplicationController < Sinatra::Base
 post '/gossips/new/' do
   puts "Ce programme ne fait rien pour le moment, on va donc afficher un message en console"
 	erb :new_gossip
@@ -23,18 +12,16 @@ end
 require_relative 'gossip'
 class ApplicationController < Sinatra::Base
 	get '/' do
-		erb :index, locals: {gossips: Gossip.all}
+		erb :new_gossip
 	end
+
+  get '/' do
+  erb :index, locals: {gossips: Gossip.all}
+end
+
 post '/gossips/new/' do
   Gossip.new(params['gossip_author'], params['gossip_content']).save
 	redirect '/'
 	end
 
-	def self.all
-  all_gossips = []
-  CSV.read("./db/gossip.csv").each do |csv_line|
-    all_gossips << Gossip.new(csv_line[0], csv_line[1])
-  end
-  return all_gossips
-end
 end
